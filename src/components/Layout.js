5ade949c381d7d7396d205893ed5dcbaa34461fe
Torii Megumi <torii.megumi@gmail.com>
2019-01-27 23:42:56 +0900

    iOS向けアイコンを追加する

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 
 import Namecard from '../components/namecard'
 import favicon from './../img/favicon.ico';
+import iOSIcon from './../img/icon-192x192.png';
 
 import './_main.sass'
 
@@ -10,7 +11,9 @@ const TemplateWrapper = ({ children }) => (
   <div className="contents--wrapper">
     <Helmet title="megumitorii.tk"
       link={[
-      { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+        { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
+        { rel: 'apple-touch-icon', href:`${iOSIcon}`, sizes:'192x192' }
+        
   ]}/>
     <Namecard />
     <div>{children}</div>
