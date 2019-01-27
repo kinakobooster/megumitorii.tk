f32d1f9c774cde63c155fdfa8dcff730c17e47ee
Torii Megumi <torii.megumi@gmail.com>
2019-01-27 23:32:29 +0900

    ファビコンを適用

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Namecard from '../components/namecard'
+import favicon from './../img/favicon.ico';
 
 import './_main.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div className="contents--wrapper">
-    <Helmet title="Home | megumitorii.tk" />
+    <Helmet title="megumitorii.tk"
+      link={[
+      { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+  ]}/>
     <Namecard />
     <div>{children}</div>
     <footer>powered by Netlify CMS and Gatsby</footer>
